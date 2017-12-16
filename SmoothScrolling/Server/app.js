a273066eddc402b5546b4f9bae85bafc91125ccc
Andrea Prearo <andrea.prearo@gmail.com>
2017-12-16 14:50:17 -0800

    Fix SmoothScrolling sample server app

--- SmoothScrolling/Server/app.js
+++ SmoothScrolling/Server/app.js
@@ -18,8 +18,4 @@ app.get('/users', function(request, response) {
   })
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-
 module.exports = app;
