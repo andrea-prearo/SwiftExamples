0d419050fb39f2e80495d1c6f747aed59692bae1
andrea-prearo <andrea.prearo@gmail.com>
2021-12-29 13:00:45 -0800

    Add CoreData <-> Codable helpers

---
+++ CoreDataCodable/CoreDataCodable/CoreData/Proxy/ManagedObjectConvertible.swift
@@ -0,0 +1,21 @@
+//
+//  ManagedObjectConvertible.swift
+//  CoreDataCodable
+//
+//  Created by Andrea Prearo on 12/29/21.
+//  Copyright © 2021 Andrea Prearo. All rights reserved.
+//
+
+import CoreData
+import Foundation
+
+/// Protocol to provide functionality for Core Data managed object conversion.
+protocol ManagedObjectConvertible {
+    associatedtype ManagedObject
+
+    /// Converts a conforming instance to a managed object instance.
+    ///
+    /// - Parameter context: The managed object context to use.
+    /// - Returns: The converted managed object instance.
+    func toManagedObject(in context: NSManagedObjectContext) -> ManagedObject?
+}
---
+++ CoreDataCodable/CoreDataCodable/CoreData/Proxy/ModelConvertible.swift
@@ -0,0 +1,19 @@
+//
+//  ModelConvertible.swift
+//  CoreDataCodable
+//
+//  Created by Andrea Prearo on 12/29/21.
+//  Copyright © 2021 Andrea Prearo. All rights reserved.
+//
+
+import Foundation
+
+/// Protocol to provide functionality for data model converion.
+protocol ModelConvertible {
+    associatedtype Model
+
+    /// Converts a conforming instance to a data model instance.
+    ///
+    /// - Returns: The converted data model instance.
+    func toModel() -> Model?
+}
