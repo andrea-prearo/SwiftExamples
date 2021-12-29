e11c77f7508a15a4fb915cc9049314f234f22dcd
andrea-prearo <andrea.prearo@gmail.com>
2021-12-29 13:01:26 -0800

    Update CoreData models

--- CoreDataCodable/CoreDataCodable/CoreData/Models/UserManagedObject.swift
+++ CoreDataCodable/CoreDataCodable/CoreData/Models/UserManagedObject.swift
@@ -37,3 +37,36 @@ class UserManagedObject: NSManagedObject {
         }
     }
 }
+
+extension UserManagedObject: ModelConvertible {
+    // MARK: - ManagedObjectDelegate
+    /// The managed entity name.
+    static var entityName = "User"
+
+    /// The entity default sort descriptors.
+    static var sortDescriptors: [NSSortDescriptor]? { return nil }
+
+    /// The default sorted fetch request.
+    static var sortedFetchRequest: NSFetchRequest<UserManagedObject> {
+        let request: NSFetchRequest<UserManagedObject> = self.fetchRequest()
+        request.sortDescriptors = UserManagedObject.sortDescriptors
+        return request
+    }
+
+    /// Returns the default fetch request (default descriptors and sorting).
+    ///
+    /// - Returns: The default fetch request.
+    static func fetchRequest() -> NSFetchRequest<UserManagedObject> {
+        return NSFetchRequest<UserManagedObject>(entityName: entityName)
+    }
+
+    // MARK: - ModelConvertible
+    /// Converts a UserManagedObject instance to a User instance.
+    ///
+    /// - Returns: The converted User instance.
+    func toModel() -> User? {
+        return User(avatarUrl: avatarUrl,
+                    username: username,
+                    role: role.rawValue)
+    }
+}
